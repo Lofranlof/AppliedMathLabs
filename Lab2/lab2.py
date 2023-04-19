79b0d045d883c59a620794fa282329c3e170e31a
Ruslan <lofranlof@gmail.com>
2023-04-19 18:23:37 +0300

    мы приматы 2

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -8,15 +8,14 @@ n = (b - a) / h  # number of points
 x = 3
 
 if __name__ == '__main__':
+    print("Function's value at the given point x:", calcFunc(x), '\n')
     print("Derivative stuff: \n")
     print("Evaluated function at given point x:", calcFunc(x))
     print("Left derivative at given point x:", leftDer(x, h))
     print("Right derivative at given point x:", rightDer(x, h))
     print("Three points derivative at given point x:", threePointsDer(x, h))
-    print("Three points derivative at the right boundary:",
-          threePointsDerRight(b, h))
-    print("Three points derivative at the left boundary:",
-          threePointsDerLeft(a, h))
+    print("Three points derivative at the right boundary:",threePointsDerRight(b, h))
+    print("Three points derivative at the left boundary:",threePointsDerLeft(a, h), "\n")
 
     print("Integral stuff: \n")
     print("Integral of the function from a to b using left rectangle method:", leftRectMethod(x, h))
